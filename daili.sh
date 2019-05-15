1f94fbee273363048d29ad7e82d77c60e6844ce3
cnnvito <50324448+cnnvito@users.noreply.github.com>
2019-05-15 10:18:37 +0800

    补充修改了一些小细节

--- daili.sh
+++ daili.sh
@@ -30,7 +30,9 @@ config_daili() {
 	}
 	EOF
 }
-<<'COMMENT'
+
+sp_sat_daili() {
+#假装批量注释
 start_daili() {
 #开启代理
 	if [ ! -e "/etc/shadowsocksr/config.json" ]; then
@@ -60,16 +62,58 @@ stop_daili() {
 	cp -af /etc/environment.stop /etc/environment
 	source /etc/environment
 }
-COMMENT
+
+stop_start() {
+case $1 in
+	start)
+		start_daili
+		exit 0
+	;;
+	stop)
+		stop_daili
+		exit 0
+	;;
+	*)
+		echo "仅支持start和stop两个参数"
+	;;
+esac
+}
+}
+
 del_daili() {
 #删除脚本配置
 	service privoxy stop
 	apt-get -y remove privoxy
 	python /usr/local/shadowsocksr/shadowsocks/local.py -c /etc/shadowsocksr/config.json -d stop
 	rm -rf /etc/shadowsocksr && rm -rf /usr/local/shadowsocksr
-	mv /etc/environment.bak /etc/environment && rm -f /etc/environment.stop /etc/environment.start
+	mv /etc/environment.bak /etc/environment
+	#rm -f /etc/environment.stop /etc/environment.start
 	source /etc/environment
 	cd /etc/init.d && update-rc.d -f privoxy.sh remove && rm -f /etc/init.d/privoxy.sh
+	
+	echo "已删除脚本配置内容，重启生效。"
+}
+
+reboot_cp() {
+	echo
+	read -p "是否重启机器？[y/n] " RB
+	case $RB in
+		[Yy])
+			echo
+			for ((j=5;j>=0;j--)); do 
+				echo -ne " $j s后自动重启\r"
+				sleep 1s
+			done
+			shutdown -r now
+		;;
+		[Nn])
+			exit 0
+		;;
+		*)
+			echo
+			echo "不知道啥意思，手动重启吧。"
+		;;
+	esac
 }
 
 #-------------------------------------------------#
@@ -87,35 +131,16 @@ fi
 
 clear
 
-#start or stop 
-<<'COMMENT'
-case $1 in
-	start)
-		start_daili
-		exit 0
-	;;
-	stop)
-		stop_daili
-		exit 0
-	;;
-	*)
-		echo "仅支持start和stop两个参数"
-	;;
-esac
-clear
-COMMENT
 echo
 echo
-echo 
-"
-***开启代理前，请在/etc/shadowsocksr/config.json填好你的\$\$节点信息。
+echo "
+***开启代理前，请在/etc/shadowsocksr/config.json填好你的\$\$r节点信息。
 
 1.我现在填写节点信息。
 
 2.我已经填好我的节点信息。
 
-3.删除代理配置
-"
+3.删除代理配置"
 echo
 while true; do
 	read -p "填好了节点信息？还是删除代理配置？输入前方数字作出你的选择: " XX
@@ -125,6 +150,7 @@ while true; do
 	;;
 	3)
 		del_daili
+		reboot_cp
 		exit 0
 	;;
 	*)
@@ -136,7 +162,7 @@ done
 echo
 echo
 echo
-for ((i=5;i>=1;i--)); do 
+for ((i=5;i>=0;i--)); do 
 	echo -ne "脚本即将运行，若填写的节点信息不正确，开启代理后将不能正常联网。$i s\r"
 	sleep 1s
 done
@@ -158,12 +184,13 @@ service privoxy restart
 #开启shadowsocksr
 python /usr/local/shadowsocksr/shadowsocks/local.py -c /etc/shadowsocksr/config.json -d start
 #全局代理写入环境变量
-cp -a /etc/environment /etc/environment.bak && cp -a /etc/environment /etc/environment.stop
+cp -a /etc/environment /etc/environment.bak 
+#cp -a /etc/environment /etc/environment.stop
 cat >> /etc/environment <<-EOF
 export http_proxy=http://127.0.0.1:8118
 export https_proxy=http://127.0.0.1:8118
 EOF
-source /etc/environment
+#source /etc/environment
 cp -a /etc/environment /etc/environment.start
 #开机自启脚本，开机直接连上代理
 cat > /etc/init.d/privoxy.sh <<-EOF
@@ -174,11 +201,15 @@ chmod 755 /etc/init.d/privoxy.sh
 cd /etc/init.d && update-rc.d privoxy.sh defaults 90
 clear
 
-echo
-"
-全局代理配置完成。
+echo "
 
-已配置开机自动代理。
+脚本发布于 愉悦人生 https://www.cnvito.top
 
-若无法联网，请检查你的节点信息是否正确。
-"
+要是脚本对你有帮助，在我的博客下面留个言可好？
+
+***全局代理配置完成***
+
+已配置开机自动代理，待重启后生效。
+
+若无法联网，请检查你的节点信息是否正确。"
+reboot_cp
