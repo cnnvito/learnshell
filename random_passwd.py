dcaa6cac37f0f552130d5c0fb4db7a83298c8dab
cnnvito <50324448+cnnvito@users.noreply.github.com>
2019-10-14 11:27:28 +0800

    Update random_passwd.py

--- random_passwd.py
+++ random_passwd.py
@@ -12,16 +12,16 @@ l2 = [2,5,9,12]
 l3 = [3,7,10,11]
 
 for n in range(0,11):
-    random_luck_math = int(random.randrange(1,12))
-    if random_luck_math in l1:
+    random_luck_num = int(random.randrange(1,12))
+    if random_luck_num in l1:
         cap = chr(random.randrange(65,91))#大写字母
         passwd.append(cap)
-    elif random_luck_math in l2:
+    elif random_luck_num in l2:
         low = chr(random.randrange(97,112))#小写字母
         passwd.append(low)
     else:
-        random_math = int(random.randrange(0,5))
-        cha = str_list[random_math] #生成随机字符
+        random_num = int(random.randrange(0,5))
+        cha = str_list[random_num] #生成随机字符
         passwd.append(cha)
 
 ret = ''.join(passwd)
